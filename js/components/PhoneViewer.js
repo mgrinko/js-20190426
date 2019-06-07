ff6443391dd283910ec732591f71fb8b2cb11549
minwuori dikova <minwuori@mail.ru>
2019-06-07 10:22:07 +0300

    imageSelected was added to phoneViewer

--- js/components/PhoneViewer.js
+++ js/components/PhoneViewer.js
@@ -3,20 +3,37 @@ export default class PhoneViewer {
     this.element = element;
     this.props = props;
 
+    this.imageSelected = null;
+
     this.state = {
-      currentPicture: this.props.phone.images[0],
+      currentPicture: (imageSelected) => {
+        let imageCollection = this.props.phone.images;
+
+        for(let i = 0; i < imageCollection.length; i++) {
+
+          if (imageSelected) {
+            return imageCollection[imageSelected];
+          } else {
+            return imageCollection[0];
+          }
+        }
+      }
     };
 
     this.render();
 
     this.element.addEventListener('click', (event) => {
-      const delegateTarget =
-        event.target.closest('[data-element="back-button"]');
-
+      const delegateTarget = event.target.closest('[data-element="back-button"]');
+      this.imageSelected = +event.target.dataset.image;
+      
+      if (this.imageSelected || this.imageSelected === 0) {
+        return this.render();
+      }
+      
       if (!delegateTarget) {
         return;
       }
-
+     
       this.props.onBack();
     });
   }
@@ -26,7 +43,7 @@ export default class PhoneViewer {
 
     this.element.innerHTML = `
       <div>
-        <img class="phone" src="${ this.state.currentPicture }">
+        <img class="phone" src="${ this.state.currentPicture(this.imageSelected) }">
     
         <button data-element="back-button">Back</button>
         <button>Add to basket</button>
@@ -37,9 +54,9 @@ export default class PhoneViewer {
         <p>${ phone.description }</p>
     
         <ul class="phone-thumbs">
-          ${ this.props.phone.images.map(phone => `
+          ${ this.props.phone.images.map((image, i) => `
           <li>
-            <img src="${phone}">
+            <img data-image= "${ i++ }" src="${ image }">
           </li>
           `).join('') } 
         </ul>
