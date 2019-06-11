240df7138de9938c11c0bad005e824528eb8af42
mgrinko <glukym@gmail.com>
2019-06-11 18:33:51 +0300

    on was added

--- js/components/PhoneViewer.js
+++ js/components/PhoneViewer.js
@@ -9,31 +9,28 @@ export default class PhoneViewer {
 
     this.render();
 
-    this.element.addEventListener('click', (event) => {
-      const delegateTarget =
-        event.target.closest('[data-element="back-button"]');
-
-      if (!delegateTarget) {
-        return;
-      }
-
-      this.props.onBack();
-    });
-
-    this.element.addEventListener('click', (event) => {
-      const delegateTarget =
-        event.target.closest('[data-element="thumbnail"]');
-
-      if (!delegateTarget) {
-        return;
-      }
-
+    this.on('click', 'back-button', this.props.onBack);
+    this.on('click', 'thumbnail', (event) => {
       this.state = {
         ...this.state,
-        currentPicture: delegateTarget.src,
+        currentPicture: event.delegateTarget.src,
       };
 
       this.render();
+    });
+  }
+
+  on(eventName, elementName, callback) {
+    this.element.addEventListener(eventName, (event) => {
+      const delegateTarget =
+        event.target.closest(`[data-element="${elementName}"]`);
+
+      if (!delegateTarget) {
+        return;
+      }
+
+      event.delegateTarget = delegateTarget;
+      callback(event);
     });
   }
 
