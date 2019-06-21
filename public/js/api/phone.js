cbe8c247398d41913c96f77d19a3c1497a10e25d
mgrinko <glukym@gmail.com>
2019-06-21 18:36:20 +0300

    Server.js was added

--- public/js/api/phone.js
+++ public/js/api/phone.js
@@ -1,4 +1,5 @@
-const API_URL = `https://mgrinko.github.io/js-20190426/api`;
+// const API_URL = `https://mgrinko.github.io/js-20190426/api`;
+const API_URL = `http://127.0.0.1:3000/api`;
 
 export const getById = async (phoneId) => {
   const response = await fetch(`${API_URL}/phones/${phoneId}.json`);
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const nodeStatic = require('node-static');
 const file = new nodeStatic.Server('.', {
   cache: 0,
   headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:63342',
+    'Access-Control-Allow-Origin': 'http://127.0.0.1:8080',
     'Access-Control-Allow-Methods': 'POST, GET',
     'Access-Control-Allow-Headers': 'Content-Type',
   }
@@ -13,6 +13,6 @@ http
   .createServer(function(req, res) {
     file.serve(req, res);
   })
-  .listen(8080);
+  .listen(3000);
 
-console.log('Server running on http://127.0.0.1:8080');
+console.log('Server running on http://127.0.0.1:3000');
