da741526bf9d7f92f994a4591988f5806385f75d
mgrinko <glukym@gmail.com>
2019-06-25 18:13:52 +0300

    Basic webpack config was added

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'none',
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  }
+};
