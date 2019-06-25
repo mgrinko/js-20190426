f0af7ab2f6e7f289bd963415eb6664edfd965539
mgrinko <glukym@gmail.com>
2019-06-25 18:26:49 +0300

    Babel loader was added

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,22 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
   }
 };
